dd39fb7fdc4fa9377bfc6e1759f38ca0e0d376df
wwml725 <wwml725@163.com>
2019-08-29 22:35:47 +0800

    0003.组件拆分完成首页静态页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,22 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import GoodsList from '@/views/GoodsList'
+import Cart from '@/views/Cart'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'GoodsList',
+      component:GoodsList
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component:Cart
+    },
   ]
 })
